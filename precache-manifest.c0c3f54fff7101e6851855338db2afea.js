83c9cbf0712fc394469f6b1dabdd8f1c5a64eb63
dzhan008 <dzhan008@ucr.edu>
2019-12-09 20:46:11 -0800

    Updates

---
+++ precache-manifest.c0c3f54fff7101e6851855338db2afea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29ef91e52ec2d3bb73a60af82132b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "328560176820eabb52d0",
+    "url": "/static/css/main.bbc11e8f.chunk.css"
+  },
+  {
+    "revision": "3b4a0bb4e6543af742e6",
+    "url": "/static/js/2.67e0823b.chunk.js"
+  },
+  {
+    "revision": "328560176820eabb52d0",
+    "url": "/static/js/main.8a0f64e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a82b71f840cb0b2f57b368ed8e2de20",
+    "url": "/static/media/ff_din_normal.0a82b71f.ttf"
+  },
+  {
+    "revision": "399ac41c0a8bf2e571248e0b7f6824b3",
+    "url": "/static/media/ff_meta_normal.399ac41c.ttf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/static/media/gilroy_light.c62aded7.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.3b87ce9c771e8b6c67537a51273ed43a.js"
+  "/precache-manifest.c0c3f54fff7101e6851855338db2afea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
