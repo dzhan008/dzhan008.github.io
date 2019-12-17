d6a80b311c3af73784be82b34704a149ecb1bcca
dzhan008 <dzhan008@ucr.edu>
2019-12-16 21:41:01 -0800

    Updates

---
+++ precache-manifest.cf63497c218d564aa0be2b82e0917d72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1582e23a2436df5ccfa4c5b1fd0ff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "819e0d8a8e144190ad85",
+    "url": "/static/css/main.5233978c.chunk.css"
+  },
+  {
+    "revision": "185aaf035c6e03993951",
+    "url": "/static/js/2.3dfe86f3.chunk.js"
+  },
+  {
+    "revision": "dd30ad2c00174e59bb32191ea757f584",
+    "url": "/static/js/2.3dfe86f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "819e0d8a8e144190ad85",
+    "url": "/static/js/main.839866c2.chunk.js"
+  },
+  {
+    "revision": "747665a1ddcd0c3e3c1e",
+    "url": "/static/js/runtime-main.1e975499.js"
+  },
+  {
+    "revision": "0a82b71f840cb0b2f57b368ed8e2de20",
+    "url": "/static/media/ff_din_normal.0a82b71f.ttf"
+  },
+  {
+    "revision": "399ac41c0a8bf2e571248e0b7f6824b3",
+    "url": "/static/media/ff_meta_normal.399ac41c.ttf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/static/media/gilroy_light.c62aded7.otf"
+  }
+]);
